b35aa8ee954115cd41b45d81d45b7cb3c90ef69a
jakeyi00 <jakeyi00@gmail.com>
2024-02-03 21:33:57 +0000

    test

---
+++ src/pages/docs.tsx
@@ -0,0 +1,11 @@
+
+import Link from "next/link";
+
+export default function Docs() {
+    return (
+        <>
+            <h1>Docs</h1>
+            <Link href="/about">About</Link>
+        </>
+    )
+}
